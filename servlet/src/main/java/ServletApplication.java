b98241ea0daef0972398bd07b6b1c70d96cf7bd2
Slava Chuprov <v.e.chuprov@gmail.com>
2022-03-29 22:11:03 +0300

    Try to add servlet to the Jetty server

--- servlet/src/main/java/ServletApplication.java
+++ servlet/src/main/java/ServletApplication.java
@@ -1,6 +1,25 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+
+
+
 public class ServletApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception{
     // run, Jetty, run!
+    int port = 8081;
+    Server server = createServer(8081);
+    server.start();
+    server.join();
+
+  }
+
+  private static Server createServer(int port) {
+    Server server = new Server(port);
+    ServletHandler handler = new ServletHandler();
+    handler.addServletWithMapping(Counter.class, "/counter");
+    server.setHandler(handler);
+    return server;
   }
 }
